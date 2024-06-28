a746cd8c5313576bbe205c3d9192a4ba7ef68d16
TylerAverette <44883616+TylerAverette@users.noreply.github.com>
2024-06-28 13:43:38 -0500

    Init
    Completed

---
+++ AnimeRepo/View/LoadingScreen.swift
@@ -0,0 +1,18 @@
+//
+//  LoadingScreen.swift
+//  AnimeRepo
+//
+//  Created by Shirley Averette on 6/27/24.
+//
+
+import SwiftUI
+
+struct LoadingScreen: View {
+    var body: some View {
+        Text("Loading!")
+    }
+}
+
+#Preview {
+    LoadingScreen()
+}
